2f58b128cd959e1873d9a5ae3bbb5e7117f6b125
NoorahOfPern <122244976+NoorahOfPern@users.noreply.github.com>
2024-09-18 16:25:51 -0500

    returned main.c to original state

--- src/main.c
+++ src/main.c
@@ -3,12 +3,6 @@
 int main( __attribute__((unused)) int argc,  __attribute__((unused)) char const *argv[])
 {
     printf("Hello World!\n");
-    
-    /* Individual Assignment 2
-    Noorah Salah
-    nhs8615@mavs.uta.edu
-    1002108615
-    */
 
     return 0;
 }
