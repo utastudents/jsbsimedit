0a56ed28c216ac05e9be5b7f1c493dbc89a877b8
Killerswin2 <thecakebecamethelie@yahoo.com>
2024-09-21 21:11:40 -0500

    Fixed: Windows post builds

--- compile_schemas.bat
+++ compile_schemas.bat
@@ -1,4 +1,4 @@
 :: windows needs to have the schemas compiled, because we are using vckpg
-build\vcpkg_installed\x64-windows\tools\glib\glib-compile-schemas.exe vcpkg_installed\x64-windows\share\glib-2.0\schemas
+vcpkg_installed\x64-windows\tools\glib\glib-compile-schemas.exe vcpkg_installed\x64-windows\share\glib-2.0\schemas
 :: XDG_DATA_DIRS needs to be set as a environment var else gtk will not find the schemas for the dialogs
-setx XDG_DATA_DIRS "%~dp0build\vcpkg_installed\x64-windows\share"
+setx XDG_DATA_DIRS "%~dp0out\build\vcpkg_installed\x64-windows\share"
