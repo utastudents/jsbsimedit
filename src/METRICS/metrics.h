e69011d6d840ef87eba5143ad68769f0fbfe4bfa
Anteneh Denbel <112484153+Antenehden@users.noreply.github.com>
2024-10-19 12:32:28 -0500

    Update metrics.h

--- src/METRICS/metrics.h
+++ src/METRICS/metrics.h
@@ -1,196 +1,94 @@
-#include "vector"
-#include "string"
-#include "memory"
-#include "map"
+#ifndef METRICS_H
+#define METRICS_H
 
-class Metrics
-{
+#include <vector>
+#include <string>
+#include <memory>
+#include <map>
+
+class Metrics {
 public:
     using string_vector = std::vector<std::string>;
+
+    // Constructor
+    Metrics();
 
 private:
     ////////////////////////////////////////////////////////
     // ------ Custom types ------
     ////////////////////////////////////////////////////////
 
-    class Unit
-    {
+    class Unit {
     public:
-        ////////////////////////////////////////////////////////
-        // ------ Constructor ------
-        Unit(string_vector &p_bank)
-        {
-            // TODO Improve copying
-            unit_bank = p_bank;
-        }
-
-        ////////////////////////////////////////////////////////
-        // ------ Methods ------
-
-        string_vector &get_unit_bank()
-        {
-            return unit_bank;
-        }
-
-        std::string get_curret_unit()
-        {
-            return current_unit;
-        }
-
-        void set_current_unit(std::string p_unit)
-        {
-            // Check if unit is in unit bank
-            current_unit = p_unit;
-        }
+        Unit(string_vector& p_bank);
+        string_vector& get_unit_bank();
+        std::string get_current_unit();
+        void set_current_unit(const std::string& p_unit);
 
     private:
-        ////////////////////////////////////////////////////////
-        // ------ Fields ------
-
-        string_vector unit_bank{};
-
-        std::string current_unit{};
+        string_vector unit_bank;
+        std::string current_unit;
     };
 
     ////////////////////////////////////////////////////////
 
-    class My_Data
-    {
+    class Data_Unit {
     public:
-        ////////////////////////////////////////////////////////
-        // ------ Constructor ------
-        My_Data(string_vector &unit_bank)
-        {
-            // TODO check this too
-            its_unit = std::make_unique<Unit>(unit_bank);
-        }
-
-        ////////////////////////////////////////////////////////
-        // ------ Methods ------
-
-        void set_value(double p_value);
-
-        double get_value()
-        {
-            return the_value;
-        }
+        Data_Unit(string_vector& unit_bank);
+        virtual void set_value(double p_value) = 0;
+        double get_value() const;
 
     protected:
-        ////////////////////////////////////////////////////////
-        // ------ Fields ------
-
-        std::unique_ptr<Unit> its_unit{nullptr};
-
+        std::unique_ptr<Unit> its_unit;
         double the_value{0};
     };
 
     ////////////////////////////////////////////////////////
 
-    class Positive_Double : public My_Data
-    {
+    class Positive_Double : public Data_Unit {
     public:
-        ////////////////////////////////////////////////////////
-        // ------ Constructor ------
-        Positive_Double(double p_value, string_vector unit_bank) : My_Data(unit_bank)
-        {
-            set_value(p_value);
-            its_unit = std::make_unique<Unit>();
-        }
-
-        ////////////////////////////////////////////////////////
-        // ------ Methods ------
-
-        void set_value(double p_value)
-        {
-            if (p_value > 0)
-                the_value = p_value;
-
-            else
-            {
-                // Throw error
-            }
-        }
+        Positive_Double(double p_value, string_vector unit_bank);
+        void set_value(double p_value) override;
     };
 
     ////////////////////////////////////////////////////////
 
-    class Norm_Double : public My_Data
-    {
+    class Norm_Double : public Data_Unit {
     public:
-        ////////////////////////////////////////////////////////
-        // ------ Constructor ------
-        Norm_Double(double p_value, string_vector unit_bank) : My_Data(unit_bank)
-        {
-            set_value(p_value);
-        }
-
-        ////////////////////////////////////////////////////////
-        // ------ Methods ------
-
-        void set_value(double p_value)
-        {
-            the_value = p_value;
-        }
+        Norm_Double(double p_value, string_vector unit_bank);
+        void set_value(double p_value) override;
     };
 
     ////////////////////////////////////////////////////////
 
-    class Vertex_Unit
-    {
+    class Vertex_Unit : public Data_Unit {
     public:
-        ////////////////////////////////////////////////////////
-        // ------ Constructor ------
-        Vertex_Unit(double p_x, double p_y, double p_z) : x(p_x), y(p_y), z(p_z)
-        {
-            // Construct Unit instance
-        }
+        Vertex_Unit(double p_x, double p_y, double p_z, string_vector unit_bank);
+        void set_x(double p_x);
+        void set_y(double p_y);
+        void set_z(double p_z);
+        double get_x() const;
+        double get_y() const;
+        double get_z() const;
 
-        ////////////////////////////////////////////////////////
-        // ------ Methods ------
-
-        // Setters
-        void set_x(double p_x)
-        {
-            x = p_x;
-        }
-        void set_y(double p_y)
-        {
-            y = p_y;
-        }
-        void set_z(double p_z)
-        {
-            z = p_z;
-        }
-
-        // Getters
-        double get_x(double p_x)
-        {
-            return p_x;
-        }
-        double get_y(double p_y)
-        {
-            return p_y;
-        }
-        double get_z(double p_z)
-        {
-            return p_z;
+        void set_value(double p_value) override {
+            the_value = p_value;  // Just a stub implementation
         }
 
     private:
-        ////////////////////////////////////////////////////////
-        // ------ Fields ------
-
         double x{0}, y{0}, z{0};
-
-        std::unique_ptr<Unit> its_unit{nullptr};
     };
 
     ////////////////////////////////////////////////////////
     // ------ Fields ------
+    ////////////////////////////////////////////////////////
 
-    std::map<std::string, std::unique_ptr<Positive_Double>> positive_data_unit{}; // 7 of these
+    std::map<std::string, std::unique_ptr<Positive_Double>> positive_data_unit;
+    std::map<std::string, std::unique_ptr<Norm_Double>> normal_data_units;
+    std::map<std::string, std::unique_ptr<Vertex_Unit>> vertex_data_units;
 
-    std::map<std::string, std::unique_ptr<Norm_Double>> normal_data_units{}; // 1 of these
-
-    std::map<std::string, std::unique_ptr<Vertex_Unit>> normal_data_units{}; // 3 of these
+    // Initialization function
+    void init();
 };
+
+#endif // METRICS_H
