969f6b28c1b94005718d046dd74f931683a5204c
Tucker Sparkman <txs8197@mavs.uta.edu>
2024-10-21 14:58:21 -0500

    XMLWrite

---
+++ src/XML/XMLWriter.cpp
@@ -0,0 +1,11 @@
+#include <XML/XMLWriter.hpp>
+
+JBSEdit::XMLWriter::XMLWriter() {
+
+}
+JBSEdit::XMLWriter::~XMLWriter() {
+
+}
+XMLDoc JBSEdit::XMLWriter::Write(const std::filesystem::path& path) {
+
+}
