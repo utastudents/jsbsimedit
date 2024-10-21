3d1b0e51c4cf72066bfe1dd751ca6acccab999e5
Killerswin2 <thecakebecamethelie@yahoo.com>
2024-10-21 13:16:30 -0500

    Changed: Application is now singleton Added: Metrics to the main file

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,22 @@
 #include <Application/Application.hpp>
 
 #include <XML/XMLDoc.hpp>
+#include <Metrics/metrics.h>
 
 
 int main( [[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
 {
     printf("Hello World!\n");
 
-    JSBEdit::Application app;
+    JSBEdit::Application& app = JSBEdit::Application::Get();
 
-    std::filesystem::path path {app.GetApplicationPath().c_str()};
+    std::filesystem::path path { Glib::get_current_dir() };
     path = std::filesystem::canonical(path.append("../../../reference/JSBSimCommander/f16.xml"));
 
-    JSBEdit::XMLDoc doc;
-    doc.LoadFileAndParse(path);
-    JSBEdit::Metrics* metric = doc.GetMetricsData();
+    Glib::RefPtr<JSBEdit::XMLDoc> doc = app.GetAircraftDoc();
+    doc->LoadFileAndParse(path);
+
+    Metrics metrics_system;
 
     //Shows the window and returns when it is closed.
     return app.Run();
