d9c5e25d596462f2093337dd9daebdd960b0da2e
Daniela Gutierrez <dxg5485@mavs.uta.edu>
2024-10-20 22:52:49 -0500

    Fixed to match with class diagram

--- src/BuoyantForces/Components.hpp
+++ src/BuoyantForces/Components.hpp
@@ -1,48 +1,59 @@
 #pragma once
 #include <iostream>
+#include <vector>
+#include "BuoyantForces/MathFunctions.hpp"
 
-class GasCell
+class Component
+{
+    public:
+        virtual ~Component() = default;
+        void setLocation(double x, double y, double z);
+        void setDimensions(double x, double y, double z);
+        void inputOverpressure(double overpressure);
+        void inputValveCoefficient(double valveCoefficient);
+        void inputInitialFullness(double fullness);
+        void selectMeasurementUnits(const std::string& unit);
+
+        enum GasType {HELIUM, HYDROGEN, AIR};
+
+    protected:
+        GasType gasType;
+        double fullness;
+        double overPressure;
+        double location[3];
+        double dimensions[3];
+        double volume;
+        double valveCoefficient;
+        double temperature;
+        double pressure;
+        double mass;
+        bool hasHeat;
+        std::vector<MathFunction> heatFunctions;
+};
+
+class GasCell : public Component
 {
     public:
         GasCell(double v, double t, double p, double m, int g);
         ~GasCell();
-        double getTemperature();
-        double getVolume();
-        double getPressure();
-        double getMass();
-        int getGasCellIndex();
 
     private:
-        double volume;
-        double temperature;
-        double pressure;
-        double mass;
-        int gasCellIndex;
+        std::vector<Ballonet> ballonets;
 };
 
-class Ballonet
+class Ballonet : public Component
 {
     public:
+        // Constructor Missing
+
+        ~Ballonet();
         int getBallonetNumber();
         void createBallonetTab(int tabIndex);
         void createBallonet(const std::string& name, const std::string& gasType);
-        void selectMeasurementUnits(const std::string& unit);
-        void setLocation(double x, double y, double z);
-        void setDimensions(double x, double y, double z);
-        void inputOverpressure(double overpressure);
-        void inputValveCoefficient(double vlaveCoefficient);
-        void inputInitialFullness(double fullness);
         void inputBlowerValve(double blowerValue);
     
     private:
+        std::string name;
         int ballonetCount;
-        std::string gasType;
-        double location[3];
-        double dimensions[3];
-        double volume;
-        double temperature;
-        double pressure;
-        double mass;
-        double fullness;
         double blowerValue;
 };
--- src/BuoyantForces/MathFunctions.hpp
+++ src/BuoyantForces/MathFunctions.hpp
@@ -10,15 +10,15 @@ class MathFunction
         void addFunction();
         void addProperty();
         void addValue();
-        void delFunction();
-        void delProperty();
-        void delValue();
+        double radiation(double emissivity, double surfaceArea, double stefanConst);
+        double conduction(double surfaceArea, double conductivity);
+        void del();     // delete method
 
     private:
-    std::string name;
-    std::string description;
-    std::vector<std::string> functionArray;
-    std::vector<std::string> propertyArray;
-    std::vector<int> valueArray;
-
+        std::string name;
+        std::string description;
+        std::string dropDownMenu;
+        std::vector<std::string> functionArray;
+        std::vector<std::string> propertyArray;
+        std::vector<double> valueArray;
 };
