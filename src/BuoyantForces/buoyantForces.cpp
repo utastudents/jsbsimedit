8e2276cbc064a71671d9b8d6671f700125a90812
Noorah Salah <noorah.salah2012@gmail.com>
2024-10-21 23:22:00 -0500

    filled in the bouyantForces.cpp from the class diagram

--- src/BuoyantForces/buoyantForces.cpp
+++ src/BuoyantForces/buoyantForces.cpp
@@ -1,1 +1,22 @@
 
+#pragma once
+#include <iostream>
+#include <vector>
+#include "BuoyantForces/Component.hpp"
+#include "BuoyantForces/bouyantForces.hpp"
+
+void buoyantForces::getGasCell(){
+    //gets the bouyant forces from the XML group
+}
+
+void buoyantForces::getBallonet(){
+    //gets the ballonett info from the objetc
+}
+
+void buoyantForces::getHeat(){
+    //gets the heatFunctions for a single object (either Balloett or GasCell) 
+}
+
+void buoyantForces::getBlower(){
+    //gets the blower MathFunction for the Ballonett
+}
