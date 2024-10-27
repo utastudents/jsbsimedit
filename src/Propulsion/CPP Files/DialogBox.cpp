3567b6424d13ecbdd626caac82868e64446a0184
sjb2148 <sjb2148@mavs.uta.edu>
2024-10-26 20:47:53 -0500

    Adding cpp files for Propulsion

---
+++ src/Propulsion/CPP Files/DialogBox.cpp
@@ -0,0 +1,10 @@
+#ifndef DIALOGBOX_H
+#define DIALOGBOX_H
+
+class DialogBox {
+public:
+    void show();
+    void close();
+};
+
+#endif // DIALOGBOX_H
---
+++ src/Propulsion/CPP Files/EngineThrusterDialog.cpp
@@ -0,0 +1,16 @@
+#include "EngineThrusterDialog.h"
+#include <iostream>
+
+EngineThrusterDialog::EngineThrusterDialog() {
+    // TODO: Initialize dialog components
+}
+
+void EngineThrusterDialog::onConfirm() {
+    // TODO: Implement confirmation logic
+    std::cout << "Confirming engine/thruster settings..." << std::endl;
+}
+
+void EngineThrusterDialog::onCancel() {
+    // TODO: Implement cancellation logic
+    std::cout << "Cancelling engine/thruster settings..." << std::endl;
+}
