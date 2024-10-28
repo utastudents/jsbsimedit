054e087b99f41d83f5a29a56abf14e1bcdf2e05c
Sujal7002 <sujalm7002@gmail.com>
2024-10-28 01:26:49 -0500

    Added InputOutput Tab Files

---
+++ src/InputOutput/MainWindow.cpp
@@ -0,0 +1,13 @@
+#include "MainWindow.h"
+
+// constructor - create and manage main window
+MainWindow::MainWindow() {}
+
+// destructor
+MainWindow::~MainWindow() {}
+
+// create and manage checkboxes
+void MainWindow::onCheckBoxToggle() {}
+
+// create and manage buttons
+void MainWindow::onButtonClicked() {}
---
+++ src/InputOutput/PopUpWindow.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "MainWindow.h"
+
+class PopUpWindow : public MainWindow {
+    public:
+        PopUpWindow(); // Constructor, initializes pop-up window components.
+        ~PopUpWindow(); // Destructor, cleans up resources when window closes
+
+    protected:
+        void onButtonClicked(); // handler for button click actions
+
+    private:
+        Gtk::Label currentPropertyValue; // Displays currently selected property
+        Gtk::Button filterButton; // Button to filter the list of properties
+        Gtk::Entry filterTextBox; // Input field for filtering properties
+        Gtk::Button showAllButton; // Button to reset and show all properties
+        Gtk::Button okButton; // OK button, confirms selection and closes pop-up
+        Gtk::Button cancelButton; // Cancel button, discards changes and closes pop-up
+        Gtk::Button closeButton; // Close button, closes the pop-up window
+        int visibleProperties; // Count of visible properties after filtering
+        int hiddenProperties; // Count of hidden properties after filtering
+        Gtk::ScrolledWindow scrolledWindowV; // Vertical scrolling window for property list
+        Gtk::ScrolledWindow scrolledWindowH; // Horizontal scrolling window for property list
+};
