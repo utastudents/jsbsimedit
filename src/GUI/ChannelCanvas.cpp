be9542e8854d2586fb937ab3c723a1d33f6909c5
Phillip Cook <pjc7731@mavs.uta.edu>
2024-10-28 23:57:03 -0500

    Class for the custom canvas

---
+++ src/GUI/ChannelCanvas.cpp
@@ -0,0 +1,59 @@
+#include "ChannelCanvas.hpp"
+
+namespace JSBEdit
+{
+
+ChannelCanvas::ChannelCanvas(const Glib::RefPtr<Gtk::Application> &app)
+{
+    set_draw_func(sigc::mem_fun(*this, &ChannelCanvas::Draw));
+    signal_show().connect([=](){ queue_draw(); });
+    set_content_height(400);
+    set_content_width(600);
+
+    //Connecting the dnd handler..
+    auto dropController = Gtk::DropTarget::create(G_TYPE_INT, Gdk::DragAction::COPY);
+    dropController->signal_drop().connect(sigc::mem_fun(*this, &ChannelCanvas::OnDragEnd), false);
+    add_controller(dropController);
+}
+
+ChannelCanvas::~ChannelCanvas()
+{
+}
+
+const std::string ChannelCanvas::GetCurrentChannelName() const
+{
+    return m_currentChannel;
+}
+
+void ChannelCanvas::SetCurrentChannelName(const std::string &channelName)
+{
+    m_currentChannel = channelName;
+    //Changed the channel so we should recall draw
+    queue_draw();
+}
+
+void ChannelCanvas::Draw(const Cairo::RefPtr<Cairo::Context> &drawCont, int width, int height)
+{
+    //Drawing the back ground this color cause why not.
+    drawCont->set_source_rgb(1.0, 1.0, 0);
+    drawCont->paint();
+
+    //This might be slow every draw, I'll probably need to make a channel class.
+    auto result = m_spriteChannels.find(m_currentChannel);
+    //Channel doesn't exist...
+    if( result == m_spriteChannels.end())
+        return;
+
+    //Channel exists, draw every sprite in the list.
+    for(auto& sprite : result->second)
+        sprite.second.Draw(drawCont);
+}
+
+bool ChannelCanvas::OnDragEnd(const Glib::ValueBase &value, double x, double y)
+{
+    //value.gobj()->data();
+    std::cout << "Something was dragged\n.";
+    return true;
+}
+
+};
