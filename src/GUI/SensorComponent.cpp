45860b14fa351b2e923b1660036b28118047a159
UTA-Wilmer <wes5481@mavs.uta.edu>
2024-10-30 15:41:00 -0500

    Sensor Component added

---
+++ src/GUI/SensorComponent.cpp
@@ -0,0 +1,11 @@
+#include "SensorComponent.hpp"
+
+SensorComponent::SensorComponent(const std::string &name, ComponentType &type)
+    : IComponentCommon(name, type)
+{
+}
+
+void SensorComponent::LoadGUI(Glib::RefPtr<Gtk::Box> &uiBox)
+{
+    //Load GUI here, add signal handlers and callbacks to uiBox.
+}
