eef566bdc3613974d32b6bfc2c7776e7d2b1cfea
UTA-Wilmer <wes5481@mavs.uta.edu>
2024-10-30 15:40:23 -0500

    Gain Component added

---
+++ src/GUI/GainComponent.cpp
@@ -0,0 +1,11 @@
+#include "GainComponent.hpp"
+
+GainComponent::GainComponent(const std::string &name, ComponentType &type)
+    : IComponentCommon(name, type)
+{
+}
+
+void GainComponent::LoadGUI(Glib::RefPtr<Gtk::Box> &uiBox)
+{
+    //Load GUI here, add signal handlers and callbacks to uiBox.
+}
