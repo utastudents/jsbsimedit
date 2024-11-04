3a51fed4aad7c3cec99908bc90e49929b2652dfb
thaiho101 <102989403+thaiho101@users.noreply.github.com>
2024-11-04 11:07:08 -0600

    Update Validation.cpp
    Implement for the following section: (author, email, limitations, and notes)

--- src/GeneralInformation/Validation.cpp
+++ src/GeneralInformation/Validation.cpp
@@ -27,12 +27,30 @@ bool Validation::validateFlightModelVersion(const std::string& flightModelVersio
 
 // Validate that the author name is non-empty and contains only alphabetic characters and spaces
 bool Validation::validateAuthor(const std::string& author) {
+    if(author.empty() || author.length() > 100 || author.find_first_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ") != std::string::npos)
+        {
+            return false;   //author input invalid
+        }
+
     return true;
 }
 
 // Validate email using a basic email regex pattern
 bool Validation::validateEmail(const std::string& email) {
-    return true;
+    size_t atPosition = email.find('@');
+    size_t dotPosition = email.find('.');
+
+    if (atPosition == std::string::npos || dotPosition == std::string::npos)
+    {
+        return false;
+    }
+
+    if ((atPosition < dotPosition) && (atPosition > 0) && (dotPosition < email.length() - 1))
+    {
+        return true;
+    }
+
+    return false;
 }
 
 // Validate that the organization name is non-empty
@@ -47,10 +65,20 @@ bool Validation::validateFileDate(const std::string& fileDate) {
 
 // Validate that the limitations are non-empty
 bool Validation::validateLimitations(const std::string& limitations) {
+    if (limitations.length() > 400)
+    {
+        return false;
+    }
+
     return true;
 }
 
 // Validate that the notes are non-empty
 bool Validation::validateNotes(const std::string& notes) {
+    if (notes.length() > 500)
+    {
+        return false;
+    }
+    
     return true;
 }
