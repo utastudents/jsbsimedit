53266a6dee5dee650f3544cc3be17c16c923ab79
KassonD <kassondavis@gmail.com>
2024-11-04 14:43:39 -0600

    testing

--- src/XML/XMLLoad.cpp
+++ src/XML/XMLLoad.cpp
@@ -3,5 +3,12 @@
 
 JSBEdit::XMLDoc JSBEdit::XMLLoad::Load(std::filesystem::path path)
 {
-    return JSBEdit::XMLDoc();
+    if (std::filesystem::exists(path))
+    {
+        pugi::xml_document doc;
+        doc.load_file(path.c_str());
+        return JSBEdit::XMLDoc(doc);
+    }
+    else
+        std::cerr << "File path does not exist: " << path << "\n";
 }
