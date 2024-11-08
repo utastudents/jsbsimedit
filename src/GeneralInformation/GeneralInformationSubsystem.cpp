6d9841fc061706e3546406a2fb0ed8fb99f42e34
Carsonb18 <cmb7216@mavs.uta.edu>
2024-11-07 21:04:16 -0600

    Removed buttons and add some text fields.

--- src/GeneralInformation/GeneralInformationSubsystem.cpp
+++ src/GeneralInformation/GeneralInformationSubsystem.cpp
@@ -19,15 +19,32 @@ void GeneralInformationSubsystem::Create()
   m_Grid.set_row_spacing(10);
   m_Grid.set_column_spacing(10);
 
-  /* this simply creates a grid of toggle buttons
-   * to demonstrate the scrolled window. */
-  for (int i = 0; i < 10; i++)
-  {
-    for (int j = 0; j < 10; j++)
-    {
-      auto s = Glib::ustring::compose("button (%1,%2)", i, j);
-      auto pButton = Gtk::make_managed<Gtk::ToggleButton>(s);
-      m_Grid.attach(*pButton, i, j);
-    }
-  }
+  //create labels, textboxes, etc.
+  auto aircraftNameLabel = Gtk::make_managed<Gtk::Label>("Aircraft Name");
+  auto aircraftNameTextbox = Gtk::make_managed<Gtk::Entry>();
+  aircraftNameTextbox->set_text("Place holder."); //load aircraft name here
+
+  auto filePathLabel = Gtk::make_managed<Gtk::Label>("File Path");
+  auto filePathTextbox = Gtk::make_managed<Gtk::Entry>();
+  filePathTextbox->set_text("Place holder."); //load file path here
+
+
+  auto releaseLevelLabel = Gtk::make_managed<Gtk::Label>("Release Level");
+  auto releaseLevelDropDown = Gtk::make_managed<Gtk::ComboBoxText>();
+  //load release levels
+  releaseLevelDropDown->append("ALPHA");
+  releaseLevelDropDown->append("BETA");
+
+  releaseLevelDropDown->set_active(0); //sets ALPHA default
+
+
+  m_Grid.attach(*aircraftNameLabel, 0, 0);
+  m_Grid.attach(*aircraftNameTextbox, 1, 0);
+
+  m_Grid.attach(*filePathLabel, 3, 0);
+  m_Grid.attach(*filePathTextbox, 4, 0);
+
+  m_Grid.attach(*releaseLevelLabel, 0, 2);
+  m_Grid.attach(*releaseLevelDropDown, 1, 2);
+
 }
