a8c23f2ea21e50effe4895f4903f3eb06e2d6e93
UTA-Wilmer <wes5481@mavs.uta.edu>
2024-11-10 00:52:21 -0600

    PID component complete, still need common for PID

--- src/GUI/PIDComponent.cpp
+++ src/GUI/PIDComponent.cpp
@@ -38,52 +38,133 @@ void PIDComponent::CreateCommonTab(Gtk::Notebook& note)
   Gtk::Box tabContainer{};
   tabContainer.set_vexpand(true);
   tabContainer.set_hexpand(true);
-  //Made a grid to go in the box
+
+  //Make grid
   Gtk::Grid g{};
   //Name layout.
+  
+  //the grid size is 11x9, where it starts from 0 
+  g.set_row_spacing(11);
+  g.set_column_spacing(9);
 
-  g.set_row_spacing(10);
-  g.set_column_spacing(15);
+  //KP label and Entry =============
+  Gtk::Label KPlabel{"KP:"};
+  m_kpEntry = Glib::RefPtr<Gtk::Entry>(new Gtk::Entry);
+  m_kpEntry->set_text(std::to_string(KP));
 
+  g.attach(KPlabel,0,1);
+  g.attach(*m_kpEntry,1,1);
 
-  Gtk::Label nameLabel{"KP:"};
-  Gtk::Entry nameEntry{};
+  //KI label and Entry =============
+  Gtk::Label KIlabel{"KI:"};
+  m_kiEntry = Glib::RefPtr<Gtk::Entry>(new Gtk::Entry);
+  m_kiEntry->set_text(std::to_string(KI));
 
-  Gtk::Label nameLabel1{"KI:"};
-  Gtk::Entry nameEntry1{};
+  g.attach(KIlabel,0,2);
+  g.attach(*m_kiEntry,1,2);
 
-  Gtk::Label nameLabel2{"KD:"};
-  Gtk::Entry nameEntry2{};
+  //KD label and Entry =============
+  Gtk::Label KDlabel{"KD:"};
+  m_kdEntry = Glib::RefPtr<Gtk::Entry>(new Gtk::Entry);
+  m_kdEntry->set_text(std::to_string(KD));
 
-  Gtk::Label nameLabel3{"Trigger:"};
-  Gtk::Entry nameEntry3{};
+  g.attach(KDlabel,0,3);
+  g.attach(*m_kdEntry,1,3);
 
+  //Trigger label and Entry ==========
+  Gtk::Label Trilabel{"Trigger:"};
+  m_trigEntry = Glib::RefPtr<Gtk::Entry>(new Gtk::Entry);
+  m_trigEntry->set_text(Trigger);
 
-//==========================================================================
-  auto check_button = Gtk::make_managed<Gtk::CheckButton>("Inverted Input");
-  g.attach(*check_button, 1,5);
-  Gtk::Label nameLabel4{"Input: "};
-  g.attach(nameLabel4, 0,4);
+  g.attach(Trilabel,0,4);
+  g.attach(*m_trigEntry,1,4);
 
-//===================================================================================
+  //Invert button click ===========
+  Gtk::Label invertLabel{"Input: "};
+  
+    //Handles the click button from check mark to not a checkmark
+  m_invertButton = Glib::RefPtr<Gtk::CheckButton>(new Gtk::CheckButton{"Invert Input"}); 
+  m_invertButton->set_active(InputInverted);
+  m_invertButton->signal_toggled().connect(sigc::mem_fun(*this, &PIDComponent::HandleInvertLogic), false);
+  
+  g.attach(invertLabel, 0,5);
+  g.attach(*m_invertButton, 1,5);
 
-  g.attach(nameLabel,0,0);// Note: (Row, Column)
-  g.attach(nameEntry,1,0);
+  //Accept/Cancel button ===========
+  m_acceptButton = Glib::RefPtr<Gtk::Button>(new Gtk::Button{"Accept"});
+  m_acceptButton->signal_clicked().connect(sigc::mem_fun(*this, &PIDComponent::SaveVariableChanges), false);
+  m_cancelButton = Glib::RefPtr<Gtk::Button>(new Gtk::Button{"Cancel"});
+  m_cancelButton->signal_clicked().connect(sigc::mem_fun(*this, &PIDComponent::DeleteWidgetData), false);
 
-  g.attach(nameLabel1,0,1);
-  g.attach(nameEntry1,1,1);
-
-  g.attach(nameLabel2,0,2);
-  g.attach(nameEntry2,1,2);
-
-  g.attach(nameLabel3,0,3);
-  g.attach(nameEntry3,1,3);
-
+  g.attach(*m_acceptButton,1,7);
+  g.attach(*m_cancelButton,2,7);
 
   //Add grid to box
   tabContainer.append(g);
   //Add tab to notebook
   note.append_page(tabContainer, "PID");
+}
+
+//When the Inverted button is active => Do something (as of now, I have no clue what Invert does)
+void PIDComponent::HandleInvertLogic()
+{
+  bool status = m_invertButton->get_active();
+}
+
+void PIDComponent::SaveVariableChanges()
+{
+  float inpFloat;
+  try{ 
+    inpFloat = std::stof(m_kpEntry->get_text());
+    KP = inpFloat;
+  }
+  catch(...){}
+
+  try{ 
+    inpFloat = std::stof(m_kiEntry->get_text());
+    KI = inpFloat;
+  }
+  catch(...){}
+
+  try{
+    inpFloat = std::stof(m_kdEntry->get_text());
+    KD = inpFloat;
+  }
+  catch(...){}
+
+  //Set the component trigger if not empty
+  auto trigEntered = m_trigEntry->get_text();
+  if(!trigEntered.empty())
+    m_componentName = trigEntered;
+
+  //Set the value of the inverted check
+  InputInverted = m_invertButton->get_active();
+
+  //Close up
+  DeleteWidgetData();
+}
+
+void PIDComponent::DeleteWidgetData()
+{
+  if(m_invertButton)
+    m_invertButton.reset();
+  if(m_kpEntry)
+    m_kpEntry.reset();
+  if(m_kiEntry)
+    m_kiEntry.reset();
+  if(m_kdEntry)
+    m_kdEntry.reset();
+  if(m_trigEntry)
+    m_trigEntry.reset();
+  if(m_acceptButton)
+    m_acceptButton.reset();
+  if(m_cancelButton)
+    m_cancelButton.reset();
+  if(m_window)
+  {
+    m_window->close();
+    m_window.reset();
+  }
 }
 
 };
--- src/GUI/PIDComponent.hpp
+++ src/GUI/PIDComponent.hpp
@@ -13,22 +13,35 @@ public:
     void LoadGUI(Glib::RefPtr<Gtk::Application>& app) override;
 
     //Member Variables, better practice to have getters/setters but lifes short to do this for all values.
+    //================Tab1
     std::string Description {};
     bool IsClipperEnabled = false;
     float MaxClip { 0.0f };
     float MinClip { 0.0f };
+    //================Tab2
     float KP { 0.0f };
     float KI { 0.0f };
     float KD { 0.0f };
     std::string Trigger {};
-    bool IsInputInverted = false;
+    bool InputInverted = false;
 
 
     private:
-    //Member vars
+    //Member functions
     void CreateCommonTab(Gtk::Notebook& note);
+    void SaveVariableChanges();
+    void DeleteWidgetData();
+    void HandleInvertLogic();
 
+    //Member Variables
     Glib::RefPtr<Gtk::Window> m_window;
+    Glib::RefPtr<Gtk::Entry> m_kpEntry{};
+    Glib::RefPtr<Gtk::Entry> m_kiEntry{};
+    Glib::RefPtr<Gtk::Entry> m_kdEntry{};
+    Glib::RefPtr<Gtk::Entry> m_trigEntry{};
+    Glib::RefPtr<Gtk::CheckButton> m_invertButton;
+    Glib::RefPtr<Gtk::Button> m_acceptButton{};
+    Glib::RefPtr<Gtk::Button> m_cancelButton{};
 };
 
 };
