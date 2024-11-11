f51861fb6aad41331de778d3598e3dd018f1116a
Zac Montgomery <zdm9277@mavs.uta.edu>
2024-11-11 16:46:54 -0600

    modified some of the comments and changed orientation a bit

--- src/MassBalance/MassBalanceSubsystem.cpp
+++ src/MassBalance/MassBalanceSubsystem.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "MassBalanceSubsystem.hpp"
-#include "Location.hpp" // Include location class
+#include "Location.hpp" 
 
 MassBalanceSubsystem::MassBalanceSubsystem() {
     m_Name = "Mass Balance";
@@ -13,23 +13,23 @@ void MassBalanceSubsystem::Create() {
     m_Grid.set_row_spacing(10);
     m_Grid.set_column_spacing(10);
 
-    // Adding three Gtk::Box widgets for x, y, and z values
+    //3 gtk boxes for x,y,z
     auto pBoxX = Gtk::make_managed<Gtk::Box>(Gtk::Orientation::HORIZONTAL);
     auto pBoxY = Gtk::make_managed<Gtk::Box>(Gtk::Orientation::HORIZONTAL);
     auto pBoxZ = Gtk::make_managed<Gtk::Box>(Gtk::Orientation::HORIZONTAL);
 
-    // Add labels to display x, y, z values (assuming x, y, z are stored in a location object)
-    Location loc; // Create a location object to hold x, y, z values
+    
+    Location loc; 
     Gtk::Label xLabel("X: " + std::to_string(loc.getX()));
     Gtk::Label yLabel("Y: " + std::to_string(loc.getY()));
     Gtk::Label zLabel("Z: " + std::to_string(loc.getZ()));
 
-    // Attach the labels to each corresponding box using append
+    //label boxes
     pBoxX->append(xLabel);
     pBoxY->append(yLabel);
     pBoxZ->append(zLabel);
 
-    // Attach these boxes to the grid layout
+    //attach to grid
     m_Grid.attach(*pBoxX, 0, 10, 1, 1);
     m_Grid.attach(*pBoxY, 1, 10, 1, 1);
     m_Grid.attach(*pBoxZ, 2, 10, 1, 1);
