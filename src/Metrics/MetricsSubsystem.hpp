11a919d970133738a770bfddbcbd3f6e36f905f3
BlaiseK <blaise.kruppa@gmail.com>
2024-11-13 00:59:52 +0000

    gui opening

--- src/Metrics/MetricsSubsystem.hpp
+++ src/Metrics/MetricsSubsystem.hpp
@@ -10,6 +10,6 @@ class MetricsSubsystem : public Subsystem
   public:
     MetricsSubsystem();
     void Create(); 
-    Metrics m_metrics; // not sure if this is good for anything, but it is the class
-                       // currently defined for this group.
+    // Metrics m_metrics; // not sure if this is good for anything, but it is the class
+    //                    // currently defined for this group.
 };
