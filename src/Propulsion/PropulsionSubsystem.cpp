f1bb6105f9d017c677c225c9930035aa586d2d20
jack yohn <jack@yohn.net>
2024-11-13 15:51:40 -0600

    Added Button names to buttons using vector with button names

--- src/Propulsion/PropulsionSubsystem.cpp
+++ src/Propulsion/PropulsionSubsystem.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <vector> // For buttonNames
+#include <map>  // For buttonMap
+#include <utility> // For std::pair
 #include "PropulsionSubsystem.hpp"
-
+#include "MainWindow.hpp"
 
 PropulsionSubsystem::PropulsionSubsystem() {
     m_Name = "Propulsion";
@@ -14,12 +17,53 @@ void PropulsionSubsystem::Create() {
   m_Grid.set_row_spacing(10);
   m_Grid.set_column_spacing(10);
 
-// i is length # j is height #
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 2; j++) {
-      auto s = Glib::ustring::compose("button (%1,%2)", i, j);
-      auto pButton = Gtk::make_managed<Gtk::ToggleButton>(s);
-      m_Grid.attach(*pButton, i, j);
+  // Replace ButtonType with the actual type of pButton
+  std::map<std::pair<int, int>, Gtk::ToggleButton*> buttonMap; 
+
+  // Seperated line of code for readability 
+  std::vector<std::string> buttonNames = {
+    // Engine Thruster Button Names
+    "Create Engine/Thruster Pair", "Engine/Thruster Pair Details", "Delete Engine/Thruster Pair", 
+    // Tank Button Names
+    "Create Tank", "Tank Details", "Delete Tank"};
+
+  int nameIndex = 0;
+
+  // i is length # j is height #
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 3; j++) {
+      if (nameIndex < buttonNames.size()) {
+        // Create each button with a unique name
+        auto pButton = Gtk::make_managed<Gtk::ToggleButton>(buttonNames[nameIndex]);
+        m_Grid.attach(*pButton, i, j);  // Place in grid (i, j) to fill bottom-right
+
+        // Store Buttons in map for accessability 
+        buttonMap[{i, j}] = pButton;
+        
+        // Assign specific actions based on button position
+        // Engine/Thruster specific buttons
+        if (i == 0 && j == 0) { // "Create Engine/Thruster Pair"
+          std::cout << "Create Engine/Thruster Pair" << std::endl;
+        } else if (i == 0 && j == 1) { // "Engine/Thruster Pair Details"
+          std::cout << "Engine/Thruster Pair Details" << std::endl;
+        }else if (i == 0 && j == 2) { // "Delete Engine/Thruster Pair"
+          std::cout << "Delete Engine/Thruster Pair" << std::endl;
+        }
+
+        // Tank specific buttons
+        if (i == 1 && j == 0) { // "Create Tank"
+          std::cout << "Create Tank" << std::endl;
+        } else if (i == 1 && j == 1) { // "Tank Details"
+          std::cout << "Tank Details" << std::endl;
+        } else if (i == 1 && j == 2) { // "Delete Tank"
+          std::cout << "Delete Tank" << std::endl;
+        }
+        
+
+        // Move to the next name
+        nameIndex++;
+      } 
+
     }
   }
 }
