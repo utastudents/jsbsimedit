917fcaad623cb652004aff255d4b8dca92484601
Faateh-UTA <mxs0048@mavs.uta.edu>
2024-11-12 22:41:36 -0600

    added column names for heirchical menu

--- src/Aerodynamics/HierarchyPanel.cpp
+++ src/Aerodynamics/HierarchyPanel.cpp
@@ -27,19 +27,25 @@ void HierarchyPanel::populateTree()
     rootRow[columns.columnName] = "Root";
 
     auto childRow1 = *(treeStore->append(rootRow.children()));
-    childRow1[columns.columnName] = "Child 1";
+    childRow1[columns.columnName] = "Drag due to...";
 
     auto childRow2 = *(treeStore->append(rootRow.children()));
-    childRow2[columns.columnName] = "Child 2";
+    childRow2[columns.columnName] = "Lift due to...";
 
     auto childRow3 = *(treeStore->append(rootRow.children()));
-    childRow3[columns.columnName] = "Child 3";
+    childRow3[columns.columnName] = "Pitch";
+
+    auto childRow4 = *(treeStore->append(rootRow.children()));
+    childRow4[columns.columnName] = "Yaw";
+
+    auto childRow5 = *(treeStore->append(rootRow.children()));
+    childRow5[columns.columnName] = "Roll";
 
     auto subChildRow1 = *(treeStore->append(childRow1.children()));
-    subChildRow1[columns.columnName] = "Subchild 1";
+    subChildRow1[columns.columnName] = "P property 1";
 
     auto subChildRow2 = *(treeStore->append(childRow2.children()));
-    subChildRow2[columns.columnName] = "Subchild 2";
+    subChildRow2[columns.columnName] = "P property 1";
 }
 
 void HierarchyPanel::changeMenu()
