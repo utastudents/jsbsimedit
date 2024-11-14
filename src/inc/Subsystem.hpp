89b36d3abe1816e67bafa7e8f6c970b6786edab5
Bud Davis <jimmie.davis@uta.edu>
2024-11-14 08:15:30 -0600

    Added new member for storing the app ptr.

--- src/inc/Subsystem.hpp
+++ src/inc/Subsystem.hpp
@@ -11,11 +11,15 @@
 class Subsystem
 {
     public:
-      Gtk::Grid m_Grid;
       // Every subsystem has to put all widgets on a Gtk::Grid .
       // All of the subsystem widgets are considered local to the 
       // subsystem.
+      Gtk::Grid m_Grid;
+      // Each subsystem has a name, that is displayed at the
+      // top of the tab
       std::string m_Name;
-      virtual void Create() = 0;
+      // some subsystems need access to the app ptr
+      Glib::RefPtr<Gtk::Application> app;
       // Create makes all the widgets, and initializes and classes.
+      virtual void Create() = 0;
 };
