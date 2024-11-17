0711de5ebe6c11d9c0e68cb16936e86ebf7b75ad
Amara <abg5609@mavs.uta.edu>
2024-11-16 19:07:31 -0600

    added rate textbox

--- src/InputOutput/MainWindow.cpp
+++ src/InputOutput/MainWindow.cpp
@@ -7,6 +7,11 @@ MainWindow::MainWindow(Gtk::Grid& m_Grid) {
     auto nameTextbox = Gtk::make_managed<Gtk::Entry>();
     m_Grid.attach(*nameLabel, 0, 0);
     m_Grid.attach(*nameTextbox, 1, 0);
+    
+    auto rateLabel = Gtk::make_managed<Gtk::Label>("Rate(*)");
+    auto rateTextbox = Gtk::make_managed<Gtk::Entry>();
+    m_Grid.attach(*rateLabel, 0, 0);
+    m_Grid.attach(*rateTextbox, 1, 0);
 }
 
 // create and manage checkboxes
