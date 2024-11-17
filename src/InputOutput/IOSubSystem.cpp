e03a92cc33cd9bd7bc378c249d23d3e2904daaf6
Sujal7002 <sujalm7002@gmail.com>
2024-11-16 21:09:35 -0600

    IO Fields Added

--- src/InputOutput/IOSubSystem.cpp
+++ src/InputOutput/IOSubSystem.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 #include "IOSubSystem.hpp"
+#include "MainWindow.hpp"
+#include <gtkmm.h>
+
 
 IOSubSystem::IOSubSystem()
 {
@@ -16,15 +19,40 @@ void IOSubSystem::Create()
   //
   /* set the spacing to 10 on x and 10 on y */
 
-  m_Grid.set_row_spacing(10);
-  m_Grid.set_column_spacing(10);
+  m_Grid.set_row_spacing(15);
+  m_Grid.set_column_spacing(20);
     
   // creates name label and text box, then attaches them to the grid
     
-  auto nameLabel = Gtk::make_managed<Gtk::Label>("Name(*)");
-  auto nameTextbox = Gtk::make_managed<Gtk::Entry>();
-  m_Grid.attach(*nameLabel, 0, 0);
-  m_Grid.attach(*nameTextbox, 1, 0);
+  // auto nameLabel = Gtk::make_managed<Gtk::Label>("Name(*)");
+  // auto nameTextbox = Gtk::make_managed<Gtk::Entry>();
+  // m_Grid.attach(*nameLabel, 0, 0);
+  // m_Grid.attach(*nameTextbox, 1, 0);
+
+  
+  nameEntry.set_text("Name(*) : ");
+  m_Grid.attach(nameEntry, 1, 0);
+  m_Grid.attach(nameTextBox, 2, 0);
+
+  typeEntry.set_text("Type(*) : ");
+  m_Grid.attach(typeEntry, 1, 1);
+  m_Grid.attach(typeDropDownList, 2, 1);
+
+  rateEntry.set_text("Rate(*) : ");
+  m_Grid.attach(rateEntry, 1, 2);
+  m_Grid.attach(rateTextBox, 2, 2);
+
+
+
+
+
+
+
+
+
+
+
+
 
   /*
   this simply creates a grid of toggle buttons
