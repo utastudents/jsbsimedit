86103df29954ac06dfc02d0460e23718122c1b24
Amara <abg5609@mavs.uta.edu>
2024-11-16 18:47:23 -0600

    fixed syntax

--- src/InputOutput/MainWindow.cpp
+++ src/InputOutput/MainWindow.cpp
@@ -5,8 +5,8 @@ MainWindow::MainWindow() {
     // creates name label and text box, then attaches them to the grid
     auto nameLabel = Gtk::make_managed<Gtk::Label>("Name(*)");
     auto nameTextbox = Gtk::make_managed<Gtk::Entry>();
-    m_Grid.attach(*nameLabel, 0, 0);
-    m_Grid.attach(*nameTextbox, 1, 0);
+    Subsystem::m_Grid.attach(*nameLabel, 0, 0);
+    Subsystem::m_Grid.attach(*nameTextbox, 1, 0);
 }
 
 // create and manage checkboxes
