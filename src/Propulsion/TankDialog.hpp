b76a6cc55ce7c115bdf0b084615e160e8ebc0146
jack yohn <jack@yohn.net>
2024-11-18 17:02:35 -0600

    Uploaded rudimentary Tank Dialog Box lacking widgets and functionality

--- src/Propulsion/TankDialog.hpp
+++ src/Propulsion/TankDialog.hpp
@@ -1,19 +1,31 @@
-#ifndef TANK_DIALOG_H
-#define TANK_DIALOG_H
+#ifndef TANK_DIALOG_HPP
+#define TANK_DIALOG_HPP
+#include <map>
+#include <string>
+#include <iostream>
+#include <gtk/gtk.h>
+#include <gtkmm.h>
+
 
 class TankDialog {
 public:
     // Constructor
     TankDialog();
-
-    // Method to handle confirmation action
+    void show(); // Show the dialog
+    
+protected:
+    // Signal handlers
     void onConfirm();
-
-    // Method to handle cancellation action
     void onCancel();
-
-    // Method to fill default values of dialog box
     void defaultValueFill();
+    void onResponse(int response_id);  // Declare onResponse method
+private:
+    // Widgets
+    Gtk::Dialog* dialogTK;
+    Gtk::Grid grid;
+
+    // TODO: Add Widgets for tank dialog box
+
 };
 
-#endif // TANK_DIALOG_H
+#endif // TANK_DIALOG_HPP
