a5ffe51b5878f875b1ecb0137aaded8fd8a95bd2
Isen Maggard <ilm0618@mavs.uta.edu>
2024-11-17 20:19:20 -0600

    subsystem update

--- src/ExternalReactions/ExternalReactionsSubsystem.cpp
+++ src/ExternalReactions/ExternalReactionsSubsystem.cpp
@@ -13,6 +13,19 @@ void ExternalReactionsSubsystem::Create() {
     m_Grid.set_row_spacing(10);
     m_Grid.set_column_spacing(10);
 
+
+    m_notebook.set_margin(10);
+    m_notebook.set_expand();
+
+    m_Grid.attach(m_notebook, 0, 0);
+
+    m_pages.push_back(std::make_unique<Gtk::Grid>());
+    //tabSetup(*m_pages.back());
+    m_notebook.append_page(*m_pages.back(), "Force 1");
+
+
+
+
     for(int i=0; i < 10; i++) {
         for (int j=0; j < 10; j++) {
             auto s = Glib::ustring::compose("button (%1 %2)", i, j);
@@ -20,4 +33,13 @@ void ExternalReactionsSubsystem::Create() {
             m_Grid.attach(*pButton, i, j);
         }
     }
+}
+
+void tabSetup(Gtk::Grid& ex_grid)
+{
+    //m_metricList = Gtk::StringList::create({"in, "})
+
+    auto forceLabel = Gtk::make_managed<Gtk::Label>("Force 1");
+
+
 }
--- src/ExternalReactions/ExternalReactionsSubsystem.hpp
+++ src/ExternalReactions/ExternalReactionsSubsystem.hpp
@@ -5,6 +5,24 @@
 
 class ExternalReactionsSubsystem : public Subsystem {
     public:
+        std::string m_Name;
         ExternalReactionsSubsystem();
         void Create();
+        void tabSetup(Gtk::Grid& ex_grid);
+
+    protected:
+        Gtk::Notebook m_notebook;
+        std::vector<std::unique_ptr<Gtk::Grid>> m_pages;
+        std::map<std::string, std::unique_ptr<Gtk::DropDown>> m_dropdown;
+        Glib::RefPtr<Gtk::StringList> m_unitList;
+
+
+    private:
+
+        double locationX;
+        double locationY;
+        double locationZ;
+        double directionX;
+        double directionY;
+        double directionZ; 
 };
