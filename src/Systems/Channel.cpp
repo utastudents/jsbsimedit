19faec6aba1f24d119c8480a8b60cdf8da50a830
Phillip Cook <pjc7731@mavs.uta.edu>
2024-11-19 02:37:07 -0600

    Now uses uids rather than strings and new window stuff

--- src/Systems/Channel.cpp
+++ src/Systems/Channel.cpp
@@ -6,12 +6,12 @@ Channel::Channel(const Glib::RefPtr<Gtk::Application> &app, const std::string & 
     : m_channelName(channelName)
 {
     m_appRef = app;
+    m_componentNameSet = std::make_shared<std::set<std::string>>();
 }
 
 bool Channel::ComponentNameExists(const std::string &name)
 {
-    //I think I should revise this to be a pair of sprites/components.
-    return (m_components.contains(name) || m_spriteComponents.contains(name)); 
+    return m_componentNameSet->contains(name); 
 }
 
 void Channel::CreateDroppedComponent(ComponentType type, int x, int y)
@@ -19,16 +19,20 @@ void Channel::CreateDroppedComponent(ComponentType type, int x, int y)
     //TODO: Create a default constructed component of the type and add to m_compoonents
     //Just doing the sprite for now.
     ComponentSprite s {type, x, y};
-    
     //Generate a unique name.
     auto name = createName(type);
     while(ComponentNameExists(name))
         name = createName(type);
+    m_componentNameSet->insert(name);
+
+    //Generate a uniqueId
+    int uId = generateUniqueId();
+    m_uniqueIDSet.insert(uId);
 
     //Insert the new sprite
-    m_spriteComponents.insert({name, s});
-    //Insert the appropriate component
-    addDefaultComponent(type, name);
+    m_spriteComponents.insert({uId, s});
+    //Insert the appropriate component, also takes care of adding the UID;
+    addDefaultComponent(type, name, uId);
 }
 
 //Maybe TODO: change this later for partial redraws if its a problem.
@@ -43,7 +47,8 @@ void Channel::Draw(const Cairo::RefPtr<Cairo::Context> &drawCont)
     drawCont->set_source_rgb(0,0,0);
     for(auto& spritePair : m_spriteComponents)
     {
-        layout->set_text(spritePair.first);
+        int uId = spritePair.first;
+        layout->set_text(m_components.at(uId)->GetName());
         //Position is oriented center, this goes to the bottom middle of box
         auto pos = spritePair.second.GetPosition();
         auto bounds = spritePair.second.GetBounds();
@@ -75,8 +80,21 @@ void Channel::HandleDoubleClick(int x, int y)
     {
         if(i.second.ContainsPoint(x, y))
         {
-            std::cout << i.first << " component was double clicked.\n";
-            m_components.at(i.first)->LoadGUI(m_appRef);
+            int uid = i.first;
+            std::cout << uid << " component was double clicked.\n";
+            auto cType = i.second.GetComponentType();
+            switch(cType)
+            {
+                case ComponentType::GAIN:
+                    m_winPtr = std::make_shared<GainComponentWindow>(m_components.at(uid), m_componentNameSet);
+                    m_appRef->add_window(*m_winPtr);
+                    break;
+
+                default:
+                    m_winPtr = std::make_shared<GainComponentWindow>(m_components.at(uid), m_componentNameSet);
+                    m_appRef->add_window(*m_winPtr);
+                    break;
+            }
             return;
         }
     }
@@ -90,30 +108,38 @@ void Channel::SetChannelName(const std::string channelName)
 std::string Channel::createName(ComponentType type)
 {
     //If theres over 1000 components in a channel we have bigger issues..
-    return std::string{COMPONENT_NAMES[static_cast<int>(type)] + std::to_string((std::rand() % 1000))};
+    return std::string{COMPONENT_NAMES[static_cast<int>(type)] + std::to_string((generateUniqueId()))};
 }
 
-void Channel::addDefaultComponent(ComponentType type, const std::string & name)
+int Channel::generateUniqueId()
+{
+    int uId = m_distribuition(m_rng);
+    if(m_uniqueIDSet.contains(uId))
+        uId = generateUniqueId();
+    return uId;
+}
+
+void Channel::addDefaultComponent(ComponentType type, const std::string & name, int uId)
 {
     switch(type)
     {
         case ComponentType::GAIN:
-            m_components.insert({name, new GainComponent{name}});
+            m_components.insert({uId, std::make_shared<GainComponent>(name)});
             break;
         case ComponentType::PID:
-            m_components.insert({name, new PIDComponent{name}});
+            m_components.insert({uId, std::make_shared<PIDComponent>(name)});
             break;
         case ComponentType::KINEMATIC:
-            m_components.insert({name, new KinematicsComponent{name}});
+            m_components.insert({uId, std::make_shared<KinematicsComponent>(name)});
             break;
         case ComponentType::SUMMER:
-            m_components.insert({name, new SummerComponent{name}});
+            m_components.insert({uId, std::make_shared<SummerComponent>(name)});
             break;
         case ComponentType::DEADBAND:
-            m_components.insert({name, new DeadbandComponent{name}});
+            m_components.insert({uId, std::make_shared<DeadbandComponent>(name)});
             break;
         default:
-            m_components.insert({name, new GainComponent{name}});
+            m_components.insert({uId, std::make_shared<GainComponent>(name)});
             break;
     }
 }
