123d5cba43769612456d18bc025e870d13a55aa0
Sujal7002 <sujalm7002@gmail.com>
2024-11-20 01:09:32 -0600

    Update

--- src/InputOutput/IOSubSystem.cpp
+++ src/InputOutput/IOSubSystem.cpp
@@ -14,10 +14,6 @@ void IOSubSystem::Create()
 {
   std::cout << "\nin IOSubsystem::Create\n" << std::endl;
 
-  //These widgets can come from a ui file or direct from code
-  //
-  /* set the spacing to 10 on x and 10 on y */
-
   m_Grid.set_row_spacing(15);
   m_Grid.set_column_spacing(20);
     
