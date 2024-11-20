8f07ad1d1c2bb3267d5054841f9f853dd252651a
Daniela Gutierrez <dxg5485@mavs.uta.edu>
2024-11-20 13:08:53 -0600

    Added entries to a map, added gascell and ballonet objects, and edited methods for dropdown menus

--- src/BuoyantForces/Component.cpp
+++ src/BuoyantForces/Component.cpp
@@ -4,18 +4,38 @@
 int Component::ballonetCount = 0;
 // Component methods
 // Constructor
-Component::Component() : maxOverpressure(0.0), 
-                         fullness(0.0), 
-                         valveCoefficient(0), 
-                         gasType(GasType::AIR), 
-                         location{0,0,0},
-	                     dimensions{0,0,0}, 
-                         volume(0.0), 
-                         temperature(0.0), 
-                         pressure(0.0), 
-                         mass(0.0),
-                         hasHeat(false)
+Component::Component() : location{0,0,0},
+	                     dimensions{0,0,0}
 {
+    maxOverpressure = 0;
+    fullness = 0;
+    valveCoefficient = 0;
+    gasType = GasType::AIR;
+    hasHeat = false;
+}
+
+void Component::setGasType(GasType type) {
+    gasType = type;
+}
+
+void Component::setLocationUnit(Unit type) { 
+    locationUnit = type;
+}
+
+void Component::setDimensionsUnit(Unit type) { 
+    dimensionsUnit = type;
+}
+
+void Component::setDimensionsShape(Unit shape) {
+    dimensionsShape = shape;
+}
+
+void Component::setPressureUnit(Unit type) { 
+    pressureUnit = type;
+}
+
+void Component::setValveCoefficientUnit(Unit type) {
+    valveCoefficientUnit = type;
 }
 
 // Set location
@@ -32,31 +52,97 @@ void Component::setDimensions(double dimensionX, double dimensionY, double dimen
     dimensions[2] = dimensionZ;
 }
 
-// Input methods
-void Component::setOverpressure(double overpressure) { maxOverpressure = overpressure; }
-void Component::setValveCoefficient(double valveCoefficient) { this->valveCoefficient = valveCoefficient; }
-void Component::setInitialFullness(double initFullness) { fullness = initFullness; }
+// Set Methods
+void Component::setOverpressure(double overpressure) { 
+    maxOverpressure = overpressure; 
+}
+
+void Component::setValveCoefficient(double valveCoefficient) { 
+    this->valveCoefficient = valveCoefficient; 
+}
+
+void Component::setInitialFullness(double initFullness) { 
+    fullness = initFullness; 
+}
+
+void Component::setName(std::string newName) {
+    name = newName;
+}
+
+// Get Methods
+Component::GasType Component::getGasType() const {
+    return gasType;
+}
+
+Component::Unit Component::getLocationUnit() const {
+    return locationUnit;
+}
+
+Component::Unit Component::getDimensionsUnit() const {
+    return dimensionsUnit;
+}
+
+Component::Unit Component::getDimensionsShape() const {
+    return dimensionsShape;
+}
+
+Component::Unit Component::getPressureUnit() const {
+    return pressureUnit;
+}
+
+Component::Unit Component::getValveCoefficientUnit() const {
+    return valveCoefficientUnit;
+}
+
+std::vector<double> Component::getLocation() const {
+    return { location[0], 
+             location[1], 
+             location[2] };
+}
+
+std::vector<double> Component::getDimensions() const {
+    return { dimensions[0], 
+             dimensions[1], 
+             dimensions[2] };
+}
+
+double Component::getOverpressure() const { 
+    return maxOverpressure; 
+}
+
+double Component::getValveCoefficient() const { 
+    return valveCoefficient; 
+}
+
+double Component::getInitialFullness() const { 
+    return fullness; 
+}
+
+std::string Component::getName() const { 
+    return name; 
+}
 
 // Unit to string conversion
 std::string Component::unitToString(Unit unit) {
     switch (unit) {
-        // include all cases for each enum type and return appropriate string
-        case Unit::WIDTH:           return "Width";
-        case Unit::RADIUS:          return "Radius";
+        case Unit::WIDTH:           return "WIDTH";
+        case Unit::RADIUS:          return "RADIUS";
+
+        case Unit::PA:              return "PA";
         case Unit::PSI:             return "PSI";
+        
         case Unit::M:               return "M";
         case Unit::IN:              return "IN";
-        case Unit::PA:              return "PA";
-        case Unit::M4_SEC_KG:       return "M4 * SEC / KG";
+        
         case Unit::FT4_SEC_SLUG:    return "FT4 * SEC / SLUG";
-        case Unit::LB_FT_SEC_R:     return "LB FT / (SEC R)";
+        case Unit::M4_SEC_KG:       return "M4 * SEC / KG";
+        
         case Unit::LBS_FT_SEC:      return "LBS FT / SEC";
+        case Unit::LB_FT_SEC_R:     return "LB FT / (SEC R)";
+        
         case Unit::FT3_SEC:         return "FT3 / SEC";
-        default: return "Unknown Unit";
     }
 }
-
-
 
 void Component::setBallonetCount(int count) 
 { 
