f4745b5db129435da5d450a1d0f95cad15a526ed
Ajay-hubb <aaa3276@mavs.uta.edu>
2024-11-20 14:40:36 -0600

    Xml handling

--- src/BuoyantForces/BuoyantForcesSubsystem.hpp
+++ src/BuoyantForces/BuoyantForcesSubsystem.hpp
@@ -1,40 +1,42 @@
 #pragma once
-
+#include "XML_api.hpp"
 #include "inc/Subsystem.hpp"
 #include <gtkmm.h>
 #include "Component.hpp"
 class BuoyantForcesSubsystem : public Subsystem
 {
-  public:
-    BuoyantForcesSubsystem();
-    void Create(); 
-    void SetupTab(Gtk::Grid& p_grid);
-    void AddUnitsDropDown(Gtk::Grid& p_grid, std::string label, int col, int row);
-    Gtk::Entry* AddEntry(Gtk::Grid& p_grid, std::string label, int col, int row, bool hasDDMenu);
-    void on_ballonetcount_changed();
-    void BuildTabs();
+public:
+  BuoyantForcesSubsystem();
+  void Create();
+  void SetupTab(Gtk::Grid &p_grid);
+  void AddUnitsDropDown(Gtk::Grid &p_grid, std::string label, int col, int row);
+  Gtk::Entry *AddEntry(Gtk::Grid &p_grid, std::string label, int col, int row, bool hasDDMenu);
+  void on_ballonetcount_changed();
+  void BuildTabs();
+  void loadXmlData();
+  void saveXmlData();
 
-  protected:
-    void on_notebook_switch_page(Gtk::Widget* page, guint page_num);
-    void on_dropdown_changed(const std::string& dropdown_name);
+protected:
+  void on_notebook_switch_page(Gtk::Widget *page, guint page_num);
+  void on_dropdown_changed(const std::string &dropdown_name);
 
-    Gtk::Notebook m_notebook;
-    Glib::RefPtr<Gtk::StringList> m_ballonetStringList;
-    std::vector<std::unique_ptr<Gtk::Grid>> m_pages;
-    std::map<std::string, std::unique_ptr<Gtk::DropDown>> m_dropdowns;
-    Glib::RefPtr<Gtk::StringList> m_gasStringList;
-    Glib::RefPtr<Gtk::StringList> m_unitsStringList;
+  Gtk::Notebook m_notebook;
+  Glib::RefPtr<Gtk::StringList> m_ballonetStringList;
+  std::vector<std::unique_ptr<Gtk::Grid>> m_pages;
+  std::map<std::string, std::unique_ptr<Gtk::DropDown>> m_dropdowns;
+  Glib::RefPtr<Gtk::StringList> m_gasStringList;
+  Glib::RefPtr<Gtk::StringList> m_unitsStringList;
 
- private:
-  	double maxOverpressure;
-    double valveCoefficient;
-    Component::Unit maxOverpressureUnit;
-    Component::Unit valveCoefficientUnit;
-    double locationX;
-    double locationY;
-    double locationZ;
-    double dimensionX;
-    double dimensionY;
-    double dimensionZ;
-    double blowerInput;
-  };
+private:
+  double maxOverpressure;
+  double valveCoefficient;
+  Component::Unit maxOverpressureUnit;
+  Component::Unit valveCoefficientUnit;
+  double locationX;
+  double locationY;
+  double locationZ;
+  double dimensionX;
+  double dimensionY;
+  double dimensionZ;
+  double blowerInput;
+};
