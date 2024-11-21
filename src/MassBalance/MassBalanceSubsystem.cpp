2ce62b452e28a6d402adf2cbdda1c3f13ad2fedc
Zac Montgomery <zdm9277@mavs.uta.edu>
2024-11-21 09:39:17 -0600

    updates to subsystem

--- src/MassBalance/MassBalanceSubsystem.cpp
+++ src/MassBalance/MassBalanceSubsystem.cpp
@@ -15,24 +15,25 @@ void MassBalanceSubsystem::Create() {
 
     // empty weight label
     auto label_empty_weight = Gtk::make_managed<Gtk::Label>("Empty Weight:");
-    m_Grid.attach(*label_empty_weight, 0, 1, 1, 1);
+    m_Grid.attach(*label_empty_weight, 1, 1, 1, 1);
+    label_empty_weight->set_halign(Gtk::Align::CENTER);
 
     //location label
     auto label_location = Gtk::make_managed<Gtk::Label>("Location:");
-    m_Grid.attach(*label_location, 0, 2, 1, 1);
-    label_location->set_halign(Gtk::Align::START);
+    m_Grid.attach(*label_location, 1, 2, 1, 1);
+    label_location->set_halign(Gtk::Align::CENTER);
 
     // box to view weight
     auto entry_empty_weight = Gtk::make_managed<Gtk::Entry>();
     entry_empty_weight->set_editable(false);
-    m_Grid.attach(*entry_empty_weight, 1, 1, 1, 1);
+    m_Grid.attach(*entry_empty_weight, 2, 1, 1, 1);
 
     // dropdown to choose lbs or kg for empty mass
     auto combo_units = Gtk::make_managed<Gtk::ComboBoxText>();
     combo_units->append("lbs");
     combo_units->append("kgs");
     combo_units->set_active(0); // Default to "lbs"
-    m_Grid.attach(*combo_units, 2, 1, 1, 1);
+    m_Grid.attach(*combo_units, 3, 1, 1, 1);
 
     
     auto pBoxX = Gtk::make_managed<Gtk::Box>(Gtk::Orientation::HORIZONTAL);
