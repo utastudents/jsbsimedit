cc27852323249e0a7f0fb7919a05cd617553bf7d
jgg9051 <172944649+jgg9051@users.noreply.github.com>
2024-11-23 17:23:05 -0600

    Update MetricsUnitTest.cpp
    Test case 1: Checks field size and data unit count

--- test/Metrics/MetricsUnitTest.cpp
+++ test/Metrics/MetricsUnitTest.cpp
@@ -1,3 +1,21 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/catch_session.hpp>
 
+#include <gtkmm.h> 
+#include <memory>
+#include "MetricsSubsystem.hpp" 
+
+TEST_CASE("MetricsSubsystem Initialization")
+{
+   MetricsSubsystem metrics;
+
+   //Check initial data units
+  REQUIRE(metrics.data_units.size() == 8); //8 data units fields
+  REQUIRE(metrics.vertex_data_units.size() == 3); //3 vertex unit fields
+
+  //validate specific data units
+  REQUIRE(metrics.data_units["wingarea"]->get_its_unit()->get_unit_bank().size() == 2); //FT2, M2
+  REQUIRE(metrics.data_units["wingarea"]->get_its_unit()->get_current_unit() == "");
+
+  REQUIRE(metrics.vertex_data_units["Aerodynamic Reference Point"]->get_its_unit()->get_unit_bank().size() == 3); //IN, FT, M
+}
