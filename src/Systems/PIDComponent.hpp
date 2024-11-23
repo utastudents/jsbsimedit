324a9d1cc721683b8302821d1b01b2c71d109f7a
UTA-Wilmer <wes5481@mavs.uta.edu>
2024-11-23 01:32:35 -0600

    Removing the old PID component

--- src/Systems/PIDComponent.hpp
+++ src/Systems/PIDComponent.hpp
@@ -18,30 +18,6 @@ public:
     bool IsClipperEnabled = false;
     float MaxClip { 0.0f };
     float MinClip { 0.0f };
-    //================Tab2
-    float KP { 0.0f };
-    float KI { 0.0f };
-    float KD { 0.0f };
-    std::string Trigger {};
-    bool InputInverted = false;
-
-
-    private:
-    //Member functions
-    void CreateCommonTab(Gtk::Notebook& note);
-    void SaveVariableChanges();
-    void DeleteWidgetData();
-    void HandleInvertLogic();
-
-    //Member Variables
-    Glib::RefPtr<Gtk::Window> m_window;
-    Glib::RefPtr<Gtk::Entry> m_kpEntry{};
-    Glib::RefPtr<Gtk::Entry> m_kiEntry{};
-    Glib::RefPtr<Gtk::Entry> m_kdEntry{};
-    Glib::RefPtr<Gtk::Entry> m_trigEntry{};
-    Glib::RefPtr<Gtk::CheckButton> m_invertButton;
-    Glib::RefPtr<Gtk::Button> m_acceptButton{};
-    Glib::RefPtr<Gtk::Button> m_cancelButton{};
 };
 
 };
