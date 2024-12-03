4832e7209170e9ed7bb10ddd57729c30e317dca7
Bud Davis <jimmie.davis@uta.edu>
2024-12-03 11:40:31 -0600

    Revert "Update MathFunction.cpp"
    This reverts commit 459cf114e136172a7de9b5a720610fa489eb1e45.

--- src/BuoyantForces/MathFunction.cpp
+++ src/BuoyantForces/MathFunction.cpp
@@ -31,14 +31,15 @@ void MathFunction::addValue(double value){
     valueArray.push_back(value);
 }
 
-double MathFunction::conduction(double surfaceArea, double conductivity) {
+void MathFunction::conduction(double surfaceArea, double conductivity) {
     double result = surfaceArea * conductivity;
-    return result;  // Return the result, conduction is not unused.
+
+
 }
 
-double MathFunction::radiation(double surfaceArea, double emissivity, double stefanConst) {
-    double result = surfaceArea * emissivity * stefanConst;
-    return result;  // Return the result, radiation is not unused.
+void MathFunction::radiation(double surfaceArea, double emissivity, double stefanConst) {
+    double result = surfaceArea * emissivity *  stefanConst;
+
 }
 
 void MathFunction:: del() {
