46c1b90b6699bc70c66548bd1863e26f97489ec3
manit0700 <manitdankhara007@gmail.com>
2024-12-04 03:21:34 +0000

    Refactored ExternalForce class and improved formatting

--- src/ExternalReactions/ExternalForce.cpp
+++ src/ExternalReactions/ExternalForce.cpp
@@ -1,33 +1,34 @@
 #include "ExternalForce.hpp"
 
+// Constructor with default values for forceVector
+Force::forceVector::forceVector(float x_val, float y_val, float z_val)
+    : x(x_val), y(y_val), z(z_val) {}
 
-    // Constructor with default values 
-    Force :: forceVector :: forceVector (float x_val, float y_val, float z_val)
-        : x(x_val), y(y_val), z(z_val) {
-        
-     }
-    // Constructor for Force class 
-    Force :: Force(forceVector direction, forceVector location)
-        : Direction (direction), Location(location), frameType(BODY){
-        
-    }
+// Constructor for Force class
+Force::Force(forceVector direction, forceVector location)
+    : Direction(direction), Location(location), frameType(BODY) {}
 
-    // Destructor 
-    Force :: ~Force() {
-        
-    }
+// Destructor
+Force::~Force() {}
 
-    void Force :: applyForce() {
-        
-    }
+// Method to apply force (currently placeholder)
+void Force::applyForce() {
+    // Logic to apply force can be added here
+}
 
-    string Force :: forceName(string name) {
-        return name;
-    }
+// Sets or gets the force name
+string Force::forceName(const string& name) {
+    // Optionally add validation logic for the name here
+    return name;
+}
 
-    double locationUnits(double units){
-        return units; 
-    }
-    void Force :: changeFrame(Frame newFrame) {
-        frameType = newFrame;
-    }
+// Converts location units (currently placeholder)
+double Force::locationUnits(double units) {
+    // Optionally add unit conversion logic here
+    return units;
+}
+
+// Changes the frame of reference
+void Force::changeFrame(const Frame& newFrame) {
+    frameType = newFrame;
+}
