6dcb7ada821e6b4948673e30a742e852f0428206
Daniela Gutierrez <dxg5485@mavs.uta.edu>
2024-12-06 17:37:21 -0600

    Changed file to use Catch2

--- test/BouyantForces/BuoyantForcesUnitTest.cpp
+++ test/BouyantForces/BuoyantForcesUnitTest.cpp
@@ -1,32 +1,24 @@
-#include <iostream>
-#include "BuoyantForces.hpp"
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_session.hpp>
+#include <BuoyantForces/BuoyantForces.hpp>
 
-int main() {
-    // Test 1: verify default state of buoyant forces
+TEST_CASE("Verify BuoyantForces Default State") {
     BuoyantForces buoyantForces;
-    if (!buoyantForces.isBuoyantForcesActive()) {
-        std::cout << "Test 1 Passed: Default buoyant forces state is inactive.\n";
-    } else {
-        std::cerr << "Test 1 Failed: Default buoyant forces state is not inactive.\n";
-    }
 
-    // Test 2: set and verify buoyant forces state
-    buoyantForces.setHasBuoyantForces(true);
-    if (buoyantForces.isBuoyantForcesActive()) {
-        std::cout << "Test 2 Passed: Buoyant forces state correctly set to active.\n";
-    } else {
-        std::cerr << "Test 2 Failed: Buoyant forces state not correctly set to active.\n";
-    }
-
-    // Test 3: set and verify gas cell
-    GasCell gasCell;
-    buoyantForces.setGasCell(gasCell);
-    if (&buoyantForces.getGasCell() == &gasCell) { // compare mem address
-        std::cout << "Test 3 Passed: GasCell correctly set and retrieved.\n";
-    } else {
-        std::cerr << "Test 3 Failed: GasCell not correctly set or retrieved.\n";
-    }
-
-    return 0;
+    REQUIRE(buoyantForces.isBuoyantForcesActive() == false);
 }
 
+TEST_CASE("Set and Verify Buoyant Forces State") {
+    BuoyantForces buoyantForces;
+
+    buoyantForces.setHasBuoyantForces(true);
+    REQUIRE(buoyantForces.isBuoyantForcesActive() == true);
+}
+
+TEST_CASE("Set and Verify Gas Cell") {
+    BuoyantForces buoyantForces;
+    GasCell gasCell;
+
+    buoyantForces.setGasCell(gasCell);
+    REQUIRE(&buoyantForces.getGasCell() == &gasCell)  // Compare Memmory Address
+}
