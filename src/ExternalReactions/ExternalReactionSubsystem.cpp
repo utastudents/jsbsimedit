dd582060217d423fe82014fbdb7809e778eba4e7
Isen Maggard <ilm0618@mavs.uta.edu>
2024-12-06 19:03:45 -0600

    cleaning up attribute names

--- src/ExternalReactions/ExternalReactionSubsystem.cpp
+++ src/ExternalReactions/ExternalReactionSubsystem.cpp
@@ -33,7 +33,7 @@ void ExternalReactionsSubsystem::Create()
     newForceButton->set_valign(Gtk::Align::START);
 
     newForceButton->signal_clicked().connect([this]()
-                                             {
+    {
         std::string forceName = m_nameTextbox->get_text();
 
         if (forceName.empty()) {
@@ -89,30 +89,30 @@ void ExternalReactionsSubsystem::Create()
     m_Grid.attach(*metricDropDownList, 3, 2);
 
     // Location Input Fields
-    auto locLabel = Gtk::make_managed<Gtk::Label>("Location:");
-    auto locXTextbox = Gtk::make_managed<Gtk::Entry>();
-    auto locYTextbox = Gtk::make_managed<Gtk::Entry>();
-    auto locZTextbox = Gtk::make_managed<Gtk::Entry>();
-    locXTextbox->set_placeholder_text("X");
-    locYTextbox->set_placeholder_text("Y");
-    locZTextbox->set_placeholder_text("Z");
-    m_Grid.attach(*locLabel, 0, 3);
-    m_Grid.attach(*locXTextbox, 1, 3);
-    m_Grid.attach(*locYTextbox, 2, 3);
-    m_Grid.attach(*locZTextbox, 3, 3);
+    auto locationLabel = Gtk::make_managed<Gtk::Label>("Location:");
+    auto locationXTextbox = Gtk::make_managed<Gtk::Entry>();
+    auto locationYTextbox = Gtk::make_managed<Gtk::Entry>();
+    auto locationZTextbox = Gtk::make_managed<Gtk::Entry>();
+    locationXTextbox->set_placeholder_text("X");
+    locationYTextbox->set_placeholder_text("Y");
+    locationZTextbox->set_placeholder_text("Z");
+    m_Grid.attach(*locationLabel, 0, 3);
+    m_Grid.attach(*locationXTextbox, 1, 3);
+    m_Grid.attach(*locationYTextbox, 2, 3);
+    m_Grid.attach(*locationZTextbox, 3, 3);
 
     // Direction Input Fields
-    auto dirLabel = Gtk::make_managed<Gtk::Label>("Direction:");
-    auto dirXTextbox = Gtk::make_managed<Gtk::Entry>();
-    auto dirYTextbox = Gtk::make_managed<Gtk::Entry>();
-    auto dirZTextbox = Gtk::make_managed<Gtk::Entry>();
-    dirXTextbox->set_placeholder_text("X");
-    dirYTextbox->set_placeholder_text("Y");
-    dirZTextbox->set_placeholder_text("Z");
-    m_Grid.attach(*dirLabel, 0, 4);
-    m_Grid.attach(*dirXTextbox, 1, 4);
-    m_Grid.attach(*dirYTextbox, 2, 4);
-    m_Grid.attach(*dirZTextbox, 3, 4);
+    auto directionLabel = Gtk::make_managed<Gtk::Label>("Direction:");
+    auto directionXTextbox = Gtk::make_managed<Gtk::Entry>();
+    auto directionYTextbox = Gtk::make_managed<Gtk::Entry>();
+    auto directionZTextbox = Gtk::make_managed<Gtk::Entry>();
+    directionXTextbox->set_placeholder_text("X");
+    directionYTextbox->set_placeholder_text("Y");
+    directionZTextbox->set_placeholder_text("Z");
+    m_Grid.attach(*directionLabel, 0, 4);
+    m_Grid.attach(*directionXTextbox, 1, 4);
+    m_Grid.attach(*directionYTextbox, 2, 4);
+    m_Grid.attach(*directionZTextbox, 3, 4);
 
     // Frame Dropdown
     auto frameLabel = Gtk::make_managed<Gtk::Label>("Frame:");
@@ -125,26 +125,26 @@ void ExternalReactionsSubsystem::Create()
     m_Grid.attach(*frameDropDownList, 1, 5);
 
     // Connect Force Type Dropdown
-    typeDropDownList->signal_changed().connect([typeDropDownList, locXTextbox, locYTextbox, locZTextbox, dirXTextbox, dirYTextbox, dirZTextbox]()
+    typeDropDownList->signal_changed().connect([typeDropDownList, locationXTextbox, locationYTextbox, locationZTextbox, directionXTextbox, directionYTextbox, directionZTextbox]()
                                                {
                                                    std::string selectedType = typeDropDownList->get_active_text();
                                                    if (selectedType == "Push-back")
                                                    {
-                                                       locXTextbox->set_text("1.0");
-                                                       locYTextbox->set_text("0.0");
-                                                       locZTextbox->set_text("0.0");
-                                                       dirXTextbox->set_text("-2.0");
-                                                       dirYTextbox->set_text("0.0");
-                                                       dirZTextbox->set_text("-1.0");
+                                                       locationXTextbox->set_text("1.0");
+                                                       locationYTextbox->set_text("0.0");
+                                                       locationZTextbox->set_text("0.0");
+                                                       directionXTextbox->set_text("-2.0");
+                                                       directionYTextbox->set_text("0.0");
+                                                       directionZTextbox->set_text("-1.0");
                                                    }
                                                    else if (selectedType == "Hook")
                                                    {
-                                                       locXTextbox->set_text("0.5");
-                                                       locYTextbox->set_text("0.5");
-                                                       locZTextbox->set_text("0.5");
-                                                       dirXTextbox->set_text("2.0");
-                                                       dirYTextbox->set_text("1.0");
-                                                       dirZTextbox->set_text("-1.0");
+                                                       locationXTextbox->set_text("0.5");
+                                                       locationYTextbox->set_text("0.5");
+                                                       locationZTextbox->set_text("0.5");
+                                                       directionXTextbox->set_text("2.0");
+                                                       directionYTextbox->set_text("1.0");
+                                                       directionZTextbox->set_text("-1.0");
                                                    }
                                                    // Additional force types can be added here
                                                });
