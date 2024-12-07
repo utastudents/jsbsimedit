5aaf2f055f24b27860eeec8fd261c335202c0a86
Ajay-hubb <aaa3276@mavs.uta.edu>
2024-12-06 18:44:55 -0600

    Ballonet Unit test

--- test/BouyantForces/BallonetUnitTest.cpp
+++ test/BouyantForces/BallonetUnitTest.cpp
@@ -2,8 +2,19 @@
 #include <catch2/catch_session.hpp>
 #include <BuoyantForces/Ballonet.hpp>
 
-TEST_CASE("Testing") {
-    Ballonet ballonet;
+// testing ballonet constructor
+TEST_CASE("Ballonet Constructor with GasType") {
+    Ballonet b("TestBallonet", Component::GasType::AIR);
 
-    // REQUIRE();
+    // initialization of blowerValue and blowerUnits
+    REQUIRE(b.getBlowerValue() == 0); // blowerValue is initialized to 0
+    REQUIRE(b.getBlowerUnit() == Component::Unit::FT3_SEC); //blowerUnits is FT3_SEC
+}
+
+TEST_CASE("Ballonet Constructor without GasType") {
+    Ballonet b("TestBallonet");
+
+    // initialization of blowerValue and blowerUnits
+    REQUIRE(b.getBlowerValue() == 0); //blowerValue is initialized to 0
+    REQUIRE(b.getBlowerUnit() == Component::Unit::FT3_SEC); //blowerUnits is FT3_SEC
 }
