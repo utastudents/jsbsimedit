30889144491aa612b2f2ad8ed474d596b591bb1f
Ajay-hubb <aaa3276@mavs.uta.edu>
2024-12-06 19:56:25 -0600

    Gas cell unit test

--- test/BouyantForces/GasCellUnitTest.cpp
+++ test/BouyantForces/GasCellUnitTest.cpp
@@ -2,8 +2,50 @@
 #include <catch2/catch_session.hpp>
 #include <BuoyantForces/GasCell.hpp>
 
-TEST_CASE("Testing") {
+TEST_CASE("GasCell Constructor") {
     GasCell gasCell;
 
-    // REQUIRE();
+    // this will check the ballonets initail state makeing sure it has 0 ballonets
+    REQUIRE(gasCell.getBallonetCount() == 0);
+    REQUIRE(gasCell.getBallonets().empty());
 }
+
+
+
+TEST_CASE("Add Ballonet to GasCell") {
+    GasCell gasCell;
+    //testing if ballonets can be added properly to gas cell
+    Ballonet ballonet1("Ballonet1", Component::GasType::AIR);
+    Ballonet ballonet2("Ballonet2", Component::GasType::HELIUM);
+
+    
+    gasCell.addBallonet(ballonet1);
+    gasCell.addBallonet(ballonet2);
+
+    // checking if added  correctly
+    REQUIRE(gasCell.getBallonetCount() == 2);
+    REQUIRE(gasCell.getBallonets().size() == 2);
+    REQUIRE(gasCell.getBallonet(0).getName() == "Ballonet1");
+    REQUIRE(gasCell.getBallonet(1).getName() == "Ballonet2");
+}
+
+TEST_CASE("Remove Last Ballonet from GasCell") {
+    GasCell gasCell;
+    //testing the  removal of ballonets
+    Ballonet ballonet1("Ballonet1", Component::GasType::AIR);
+    Ballonet ballonet2("Ballonet2", Component::GasType::HELIUM);
+
+    //adding ballonets
+    gasCell.addBallonet(ballonet1);
+    gasCell.addBallonet(ballonet2);
+
+    // Removing last added ballonet
+    gasCell.removeLastBallonet();
+
+    // check if deleted
+    REQUIRE(gasCell.getBallonetCount() == 1);
+    REQUIRE(gasCell.getBallonets().size() == 1);
+    REQUIRE(gasCell.getBallonet(0).getName() == "Ballonet1");
+}
+
+
